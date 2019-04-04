e2db544a1cd7e0301772beae52a89bc3e46c82f7
Jack Sulis <c3331236@uon.edu.au>
2019-04-04 05:36:07 +0000

    End of week 6 lab

--- main.c
+++ main.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
 
-int main() {
+int task1(int r, char l);
 
+int main() {
+    int tasknum;
+    printf("Select Task: \n");
+    printf("1. \n");
+    printf("2. \n");
+    printf("3. \n");
+    printf("4. \n");
+    printf("5. \n");
+    printf("6. \n");
+    
+    
+    switch(tasknum) {
+        case 1:
+            printf("1");
+            break;
+        case 2:
+            printf("2");
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        default:
+            printf("Please enter a number from 1-6");
+        
+    }
     return 0;
+}
+
+int task1(int r, char l) {
+    
+    
 }
